a33c6216cdfc6b387cdff2a42ea4a5346d99d07c
Mohammad Anwari <mdamt@mdamt.net>
2014-10-01 14:13:08 +0700

    Check both SSHA and mongodb/bcrypt hashes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+var bcrypt = require ("nan-bcrypt");
 var config = require("./config");
 var mongoose = require ("mongoose");
-var User = require("./schema");
+var User = require("./schema").User;
+var Domain = require("./schema").Domain;
 var ssha = require("openldap_ssha");
 var fs = require("fs");
 var bsplit = require('buffer-split');
@@ -17,26 +19,53 @@ var notAuthenticated = function () {
   process.exit(1);
 }
 
-var start = function(username, password) {
-  var task = User.findOne({email:username});
-  task.exec(function(e, result) {
-    if (result) {
-      ssha.checkssha(password, result.hash, function(err, passOk) {
+var start = function(email, password) {
+  var id = email.split("@");
+
+  var username = id[0];
+  var domain = id[1];
+
+  if (!(username && domain)) {
+    return notAuthenticated();
+  }
+
+  var domainQuery = Domain.findOne({name: domain});
+  domainQuery.exec(function(e, domainResult) {
+    if (!domainResult) {
+      return notAuthenticated();
+    }
+    var task = User.findOne({
+      username: username,
+      domain: domainResult._id
+    });
+    task.exec(function(e, result) {
+      var success = function(passOk) {
         if (!passOk) {
           notAuthenticated();
         }
         var arg = "";
-        arg += "HOME=" + config.home + "/" + username + "\t";
+        arg += "HOME=" + config.home + "/" + email + "\t";
         arg += "USER=" + username + "\t";
 
         var w = fs.createWriteStream(null, {fd: 4});
         w.write(arg);
         w.end();
         process.exit(0);
-      });
-    } else {
-      notAuthenticated();
-    }
+      }
+
+      if (result) {
+        if (result.hash.indexOf("{SSHA}") == 0) {
+          ssha.checkssha(password, result.hash, function(err, passOk) {
+            success(passOk);
+          });
+        } else {
+          var passOk = bcrypt.compareSync(password, result.hash);
+          success(passOk);
+        }
+      } else {
+        notAuthenticated();
+      }
+    });
   });
 }
 
